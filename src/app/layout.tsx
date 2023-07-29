0a50558fc5476a35f7e6c6f0b3c89910535a9b6f
Ayush <95138683+astrobutter@users.noreply.github.com>
2023-07-29 21:15:35 +0530

    ..
    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,15 @@ import NavBar from "./components/Navbar";
 export const metadata = {
   title: "Car Hub",
   description: "Discover world's best car showcase application",
+  
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <head>
+      <link rel="icon" href="/icon-car.ico" />
+      </head>
       <body className='relative'>
         <NavBar />
         {children}
